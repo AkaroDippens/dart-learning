a55050d7e73c5a4f05dc5cddee5d9cb6051e002b
AkaroDippens <mirzoev.nidzhat@mail.ru>
2024-12-03 00:44:08 +0300

    Adding async functions and files in Battleship game

--- .idea/ship/PastShipMain.dart
+++ .idea/ship/PastShipMain.dart
@@ -1,10 +1,9 @@
 import 'dart:io';
 import 'dart:math';
-import 'dart:isolate';
 
-void main() async {
+void main() {
   final game = Game();
-  await game.start();
+  game.start();
 }
 
 // Класс для корабля
@@ -24,8 +23,6 @@ class GameField {
   final int size;
   List<List<String>> grid;
   List<Ship> ships = [];
-  int hits = 0;
-  int misses = 0;
 
   GameField(this.size)
       : grid = List.generate(size, (_) => List.generate(size, (_) => '.')); // Инициализация игрового поля
@@ -76,7 +73,7 @@ class GameField {
   }
 
   // Метод для вывода поля с координатами
-  void printFieldWithCoordinates({bool hideShips = false})  {
+  void printFieldWithCoordinates({bool hideShips = false}) {
     stdout.write('  ');
     // Выводим номера столбцов
     for (int i = 0; i < size; i++) {
@@ -157,7 +154,6 @@ class GameField {
   bool receiveAttack(int row, int col) {
     if (grid[row][col] == 'S') {
       grid[row][col] = 'X';
-      hits++;
       for (var ship in ships) {
         for (var pos in ship.positions) {
           if (pos[0] == row && pos[1] == col) {
@@ -172,7 +168,6 @@ class GameField {
       }
     } else if (grid[row][col] == '.') {
         grid[row][col] = 'O';
-        misses++;
         print('Мимо!');
         return true;
     }
@@ -215,17 +210,14 @@ class Player {
     }
   }
 
-  Future<void> makeMove(Player opponent) async {
+  void makeMove(Player opponent) {
     if (isBot) {
       final random = Random();
       while (true) {
         final row = random.nextInt(field.size);
         final col = random.nextInt(field.size);
-        if (await opponent.field.receiveAttack(row, col)) {
-          await logMove(name, row, col, 'Мимо');
+        if (opponent.field.receiveAttack(row, col)) {
           break;
-        } else {
-          await logMove(name, row, col, 'Попадание');
         }
       }
     } else {
@@ -239,16 +231,8 @@ class Player {
         final col = int.tryParse(parts[1]);
         if (row == null || col == null) continue;
 
-        try {
-          if (await opponent.field.receiveAttack(row, col)) {
-            await logMove(name, row, col, 'Мимо');
-            break;
-          } else {
-            await logMove(name, row, col, 'Попадание');
-          }
-        } catch (e) {
-          await logError(name, row, col, e.toString());
-          print(e);
+        if (opponent.field.receiveAttack(row, col)) {
+          break;
         }
       }
     }
@@ -260,10 +244,10 @@ class Game {
   late Player player1;
   late Player player2;
 
-  Future<void> start() async {
+  void start() {
     print('Добро пожаловать в Морской Бой!');
     print('Выберите размер поля: 1) 10x10 2) 12x12 3) 14x14');
-    final fieldSize = await _selectFieldSize();
+    final fieldSize = _selectFieldSize();
 
     print('Введите имя игрока 1:');
     final player1Name = stdin.readLineSync()!;
@@ -274,14 +258,14 @@ class Game {
     player1 = Player(player1Name, fieldSize, false);
     player2 = Player(player2Name, fieldSize, playAgainstBot);
 
-    final ships = await _createShips(fieldSize);
+    final ships = _createShips(fieldSize);
     player1.placeShips(ships);
     player2.placeShips(ships);
 
-    await _gameLoop();
+    _gameLoop();
   }
 
-  Future<int> _selectFieldSize() async {
+  int _selectFieldSize() {
     while (true) {
       final input = stdin.readLineSync();
       if (input == '1') return 10;
@@ -291,7 +275,7 @@ class Game {
     }
   }
 
-  Future<List<Ship>> _createShips(int fieldSize) async {
+  List<Ship> _createShips(int fieldSize) {
     final shipSizes = fieldSize == 10
         ? [5, 4, 3, 3, 2]
         : fieldSize == 12
@@ -301,25 +285,22 @@ class Game {
         (i) => Ship('Корабль ${i + 1}', shipSizes[i]));
   }
 
-  Future<void> _gameLoop() async {
+  void _gameLoop() {
     var currentPlayer = player1;
     var opponent = player2;
 
     while (true) {
-      print('\x1B[2J\x1B[0;0H');
+      print('\x1B[2J\x1B[0;0H');;
       print('Ходит игрок: ${currentPlayer.name}');
       print('Ваше поле:');
       currentPlayer.field.printField();
       print('Поле соперника:');
       opponent.field.printField(hideShips: true);
 
-      await currentPlayer.makeMove(opponent);
+      currentPlayer.makeMove(opponent);
 
       if (opponent.field.ships.every((s) => s.isSunk())) {
         print('${currentPlayer.name} победил!');
-        await updatePlayerStats(currentPlayer.name, 1, 1, 0);
-        await updatePlayerStats(opponent.name, 1, 0, 1);
-        await createGameLog(player1, player2);
         break;
       }
 
@@ -328,57 +309,5 @@ class Game {
       currentPlayer = opponent;
       opponent = temp;
     }
-  }
-}
-
-Future<void> updatePlayerStats(String playerName, int gamesPlayed, int wins, int losses) async {
-  final file = File('players/$playerName.txt');
-  await _ensureDirectoryExists(file.parent);
-  if (await file.exists()) {
-    final lines = await file.readAsLines();
-    gamesPlayed += int.parse(lines[1]);
-    wins += int.parse(lines[2]);
-    losses += int.parse(lines[3]);
-  }
-  await file.writeAsString('$playerName\n$gamesPlayed\n$wins\n$losses');
-}
-
-Future<void> createGameLog(Player player1, Player player2) async {
-  final file = File('games/${player1.name}-${player2.name}.txt');
-  await _ensureDirectoryExists(file.parent);
-  final player1ShipsIntact = player1.field.ships.where((s) => !s.isSunk()).length;
-  final player2ShipsIntact = player2.field.ships.where((s) => !s.isSunk()).length;
-  final player1ShipsHit = player1.field.ships.where((s) => s.hits > 0 && !s.isSunk()).length;
-  final player2ShipsHit = player2.field.ships.where((s) => s.hits > 0 && !s.isSunk()).length;
-  final player1ShipsSunk = player1.field.ships.where((s) => s.isSunk()).length;
-  final player2ShipsSunk = player2.field.ships.where((s) => s.isSunk()).length;
-
-  await file.writeAsString('''
-${player1.name}: ${player1.field.hits} попаданий, ${player1.field.misses} промахов, $player1ShipsIntact кораблей целы, $player1ShipsHit подбито, $player1ShipsSunk потоплено
-${player2.name}: ${player2.field.hits} попаданий, ${player2.field.misses} промахов, $player2ShipsIntact кораблей целы, $player2ShipsHit подбито, $player2ShipsSunk потоплено
-''');
-}
-
-Future<void> clearGameLog(String player1Name, String player2Name) async {
-  final file = File('games/$player1Name-$player2Name.txt');
-  await _ensureDirectoryExists(file.parent);
-  await file.delete();
-}
-
-Future<void> logMove(String playerName, int row, int col, String result) async {
-  final file = File('logs/game.log');
-  await _ensureDirectoryExists(file.parent);
-  await file.writeAsString('$playerName сделал ход на $row$col, результат: $result\n', mode: FileMode.append);
-}
-
-Future<void> logError(String playerName, int row, int col, String error) async {
-  final file = File('logs/errors.log');
-  await _ensureDirectoryExists(file.parent);
-  await file.writeAsString('$playerName пытался сделать ход на $row$col, ошибка: $error\n', mode: FileMode.append);
-}
-
-Future<void> _ensureDirectoryExists(Directory directory) async {
-  if (!await directory.exists()) {
-    await directory.create(recursive: true);
   }
 }
